d77e2ddca1a77c148e16002f0a94d3ec6db7e027
Maike <maike_jbe@hotmail.com>
2018-08-07 18:33:12 -0300

    Alteração de passagem de parâmetro

--- sources/Builders/FromArray.php
+++ sources/Builders/FromArray.php
@@ -26,32 +26,8 @@ class FromArray implements Builder
         ) {
             throw new Exception('ciebit.news.builders.invalid', 3);
         }
-        $story = (new StoryBuilder)->setData([
-            'title' => $this->data['story_title'],
-            'status' => $this->data['story_status'],
-            'body' => $this->data['story_body'],
-            'date_hour' => $this->data['story_date_hour'],
-            'id' => $this->data['story_id'],
-            'summary' => $this->data['story_summary'],
-            'uri' => $this->data['story_uri'],
-            'views' => $this->data['story_views']
-        ])->build();
-
-        $image = (new ImageBuilder)->setData([
-            'id' => $this->data['image_id'],
-            'name' => $this->data['image_name'],
-            'caption' => $this->data['image_caption'],
-            'description' => $this->data['image_description'],
-            'uri' => $this->data['image_uri'],
-            'extension' => $this->data['image_extension'],
-            'size' => $this->data['image_size'],
-            'views' => $this->data['image_views'],
-            'variations' => $this->data['image_variations'],
-            'mimetype' => $this->data['image_mimetype'],
-            'date_hour' => $this->data['image_date_hour'],
-            'metadata' => $this->data['image_metadata'],
-            'status' => $this->data['image_status']
-        ])->build();
+        $story = (new StoryBuilder)->setData($this->data['story'])->build();
+        $image = (new ImageBuilder)->setData($this->data['image'])->build();
 
         $news = new News(
             $story,
