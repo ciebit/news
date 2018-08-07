7846d88fe2260b113c0ad34decb162a6bdb23d0d
Maike <maike_jbe@hotmail.com>
2018-08-07 16:04:28 -0300

    Passada a responsabilidade de criação dos objetos Story e Image para seus respectivos Builders

--- sources/Builders/Strategies/FromArray.php
+++ sources/Builders/Strategies/FromArray.php
@@ -5,6 +5,8 @@ namespace Ciebit\News\Builders\Strategies;
 
 use Ciebit\News\News;
 use Ciebit\News\Status;
+use Ciebit\Stories\Builders\FromArray as StoryBuilder;
+use Ciebit\Files\Images\Builders\FromArray as ImageBuilder;
 use DateTime;
 
 class FromArray implements Strategy
@@ -25,24 +27,32 @@ class FromArray implements Strategy
         ) {
             throw new Exception('ciebit.news.builders.invalid', 3);
         }
-        $story = (new Story(
-            $this->data['story_title'],
-            $this->data['story_status']
-        ));
-        isset($this->data['story_body'])
-        && $story->setBody($this->data['story_body']);
-        isset($this->data['story_date_hour'])
-        && $story->setDateTime($this->data['story_date_hour']);
-        isset($this->data['story_id'])
-        && $story->setId($this->data['story_id']);
-        isset($this->data['story_summary'])
-        && $story->setSummary($this->data['story_summary']);
-        isset($this->data['story_uri'])
-        && $story->setUri($this->data['story_uri']);
-        isset($this->data['story_views'])
-        && $story->setViews($this->data['story_views']);;
+        $story = (new StoryBuilder)->setData([
+            'title' => $this->data['story_title'],
+            'status' => $this->data['story_status'],
+            'body' => $this->data['story_body'],
+            'date_hour' => $this->data['story_date_hour'],
+            'id' => $this->data['story_id'],
+            'summary' => $this->data['story_summary'],
+            'uri' => $this->data['story_uri'],
+            'views' => $this->data['story_views']
+        ])->build();
 
-        $image = new Image;
+        $image = (new ImageBuilder)->setData([
+            'id' => $this->data['image_id'],
+            'name' => $this->data['image_name'],
+            'caption' => $this->data['image_caption'],
+            'description' => $this->data['image_description'],
+            'uri' => $this->data['image_uri'],
+            'extension' => $this->data['image_extension'],
+            'size' => $this->data['image_size'],
+            'views' => $this->data['image_views'],
+            'variations' => $this->data['image_variations'],
+            'mimetype' => $this->data['image_mimetype'],
+            'date_hour' => $this->data['image_date_hour'],
+            'metadata' => $this->data['image_metadata'],
+            'status' => $this->data['image_status']
+        ])->build();
 
         $news = new News(
             $story,
