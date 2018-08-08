d1886ca19e3ab928af15257fd2f12505d2795dae
Johnny Saymon <saymon@ciebit.com.br>
2018-08-08 11:49:06 -0300

    Adicionado criacao da view no script SQL

--- database/v1.0.sql
+++ database/v1.0.sql
@@ -8,3 +8,32 @@ CREATE TABLE `cb_news` (
   `status` tinyint(1) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='version:1.0';
+
+CREATE VIEW cb_news_complete AS
+SELECT
+    `news`.`id`,
+    `news`.`status`,
+    `stories`.`id` as `story_id`,
+    `stories`.`title` as `story_title`,
+    `stories`.`summary` as `story_summary`,
+    `stories`.`body` as `story_body`,
+    `stories`.`datetime` as `story_datetime`,
+    `stories`.`uri` as `story_uri`,
+    `stories`.`views` as `story_views`,
+    `stories`.`status` as `story_status`,
+    `files`.`id` as `cover_id`,
+    `files`.`name` as `cover_name`,
+    `files`.`description` as `cover_description`,
+    `files`.`uri` as `cover_uri`,
+    `files`.`extension` as `cover_extension`,
+    `files`.`size` as `cover_size`,
+    `files`.`views` as `cover_views`,
+    `files`.`mimetype` as `cover_mimetype`,
+    `files`.`date_hour` as `cover_date_hour`,
+    `files`.`metadata` as `cover_metadata`,
+    `files`.`status` as `cover_status`
+FROM `cb_news` AS `news`
+INNER JOIN `cb_stories` AS `stories`
+	ON `stories`.`id` = `news`.`story_id`
+LEFT JOIN `cb_files` AS `files`
+	ON `files`.`id` = `news`.`cover_id`
