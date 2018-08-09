8fa723d2baf5b33f99cd7605ab525ff6863b905f
Maike <maike_jbe@hotmail.com>
2018-08-09 10:29:04 -0300

    Adição de métodos e propriedade

--- sources/News.php
+++ sources/News.php
@@ -7,15 +7,22 @@ use Ciebit\Stories\Story;
 
 class News
 {
+    private $id; #:int
     private $story; #:Story
     private $image; #:?Image
     private $status; #:Status
 
     public function __construct(Story $story, Image $image = null, Status $status)
     {
+        $this->id = 0;
         $this->image = $image;
         $this->status = $status;
         $this->story = $story;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     public function getCover(): ?Image
@@ -31,5 +38,11 @@ class News
     public function getStory(): Story
     {
         return $this->story;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
     }
 }
