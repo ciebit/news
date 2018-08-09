e9b04b73a163363b1e7801b85399860695c4e86f
Maike <maike_jbe@hotmail.com>
2018-08-09 10:03:14 -0300

    Organizadas constantes em ordem crescente

--- sources/Status.php
+++ sources/Status.php
@@ -5,9 +5,9 @@ use MyCLabs\Enum\Enum;
 
 class Status extends Enum
 {
-    const ACTIVE = 3;
-    const ANALYZE = 2;
     const DRAFT = 1;
+    const ANALYZE = 2;
+    const ACTIVE = 3;
     const TRASH = 4;
     const INACTIVE = 5;
 }
